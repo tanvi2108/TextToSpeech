8e88466b3dab95b5aa55470ded9c65d52cf4381c
tanvi2108 <tanvi.panchal96@gmail.com>
2021-12-21 20:54:38 -0500

    UI changes

---
+++ text-to-speech/src/components/Frontview.vue
@@ -0,0 +1,50 @@
+<template>
+<div id="app">
+<h3>Enter your text</h3>
+<form>
+  <div class="form-group">
+    <label for="text">Enter the text </label>
+    <input type="text" class="form-control" id="exampleInputText" placeholder="Enter your text here">
+    
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</template>
+<script></script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Castoro&display=swap');
+#app {
+font-family: 'Lato', sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+color: #2c3e50;
+}
+#input-group-1 {
+margin-left:25%;
+margin-bottom: 20px;
+width: 50%;
+padding-left: 80px;
+margin-top: 50px;
+font-weight:bold;
+}
+h3{
+margin-left:40%;
+margin-top:20px;
+font-family: 'Castoro', serif;
+}
+
+#button-1{
+margin-left:50%;
+}
+.nav-bar{
+padding-left: 50px;;
+}
+.link{
+padding-right:100px;
+font-size: 20px;
+color:white;
+}
+</style>
